63990e4fb0b0b801fc5ea05a252d1d1426324130
WonCheolHwang <wch18735@naver.com>
2024-05-03 16:59:34 +0900

    [FIX] Mock 테스트를 위한 Nested 테스트 그룹 해체

--- src/test/java/ssd/SamsungSSDTest.java
+++ src/test/java/ssd/SamsungSSDTest.java
@@ -19,9 +19,12 @@ class SamsungSSDTest {
 
     DeviceDriver deviceDriver;
 
+    SSDInterface ssd;
+
     @BeforeEach
     void setUp() {
         deviceDriver = new DeviceDriver(ssdInterface);
+        ssd = new SamsungSSD();
     }
 
     @Test
@@ -42,55 +45,45 @@ class SamsungSSDTest {
         verify(ssdInterface, times(1)).read("3");
     }
 
-    @Nested
-    class SamsungSSDWrite {
-        SamsungSSD ssd;
+    @Test
+    @DisplayName("SamsungSSD 객체가 정상적으로 생성된다")
+    void createSamsungSSD(){
+        assertThat(ssd).isNotNull();
+    }
 
-        @BeforeEach
-        void setUp() {
-            ssd = new SamsungSSD();
+    @Test
+    @DisplayName("data 에 16진수 값이 넘는 값이 전달되면 RuntimeException 을 던진다")
+    void writeThrowRuntimeExceptionWhenDataValueExceedInteger(){
+        try {
+            // when
+            ssd.write("3", "0x1298CDXF");
+            fail();
+        } catch(RuntimeException e) {
+            // then
+            assertThat(e).isInstanceOf(RuntimeException.class);
         }
+    }
 
-        @Test
-        @DisplayName("SamsungSSD 객체가 정상적으로 생성된다")
-        void createSamsungSSD(){
-            assertThat(ssd).isNotNull();
+    @Test
+    @DisplayName("lba 에 0보다 작은 값이 전달되는 경우 RuntimeException 을 던진다")
+    void writeThrowRuntimeExceptionWhenLbaValueExceedInteger(){
+        try {
+            // when
+            ssd.write("-1", "0x12345678");
+            fail();
+        } catch(RuntimeException e) {
+            // then
+            assertThat(e).isInstanceOf(RuntimeException.class);
         }
+    }
 
-        @Test
-        @DisplayName("data 에 16진수 값이 넘는 값이 전달되면 RuntimeException 을 던진다")
-        void writeThrowRuntimeExceptionWhenDataValueExceedInteger(){
-            try {
-                // when
-                ssd.write("3", "0x1298CDXF");
-                fail();
-            } catch(RuntimeException e) {
-                // then
-                assertThat(e).isInstanceOf(RuntimeException.class);
-            }
-        }
-
-        @Test
-        @DisplayName("lba 에 0보다 작은 값이 전달되는 경우 RuntimeException 을 던진다")
-        void writeThrowRuntimeExceptionWhenLbaValueExceedInteger(){
-            try {
-                // when
-                ssd.write("-1", "0x12345678");
-                fail();
-            } catch(RuntimeException e) {
-                // then
-                assertThat(e).isInstanceOf(RuntimeException.class);
-            }
-        }
-
-        @Test
-        @DisplayName("data 값에 prefix 0x 값이 아닌 경우 RuntimeException")
-        void writeThrowRuntimeExceptionWhenPrefixValueIsNot0x(){
-            try {
-                ssd.write("1", "0012345678");
-            } catch(RuntimeException e) {
-                assertThat(e).isInstanceOf(RuntimeException.class);
-            }
+    @Test
+    @DisplayName("data 값에 prefix 0x 값이 아닌 경우 RuntimeException")
+    void writeThrowRuntimeExceptionWhenPrefixValueIsNot0x(){
+        try {
+            ssd.write("1", "0012345678");
+        } catch(RuntimeException e) {
+            assertThat(e).isInstanceOf(RuntimeException.class);
         }
     }
 }
