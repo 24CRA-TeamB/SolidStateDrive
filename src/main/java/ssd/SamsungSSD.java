2866bfe406949a7f2c94bb836ff40a1ebde47df6
WonCheolHwang <wch18735@naver.com>
2024-05-03 16:01:12 +0900

    [TC] data에 비정상 값이 전달되면 RuntimeException 발생 (Green State)

--- src/main/java/ssd/SamsungSSD.java
+++ src/main/java/ssd/SamsungSSD.java
@@ -9,6 +9,17 @@ public class SamsungSSD implements SSDInterface{
 
     @Override
     public void write(String lba, String data) {
-        //File Write
+        for(int i = 2; i < data.length(); i++){
+            if(isHexaDecimal(data.charAt(i))) {
+                throw new RuntimeException();
+            }
+        }
+
+        if(Integer.parseInt(lba) < 0)
+            throw new RuntimeException();
+    }
+
+    private static boolean isHexaDecimal(char c) {
+        return !(('0' <= c && c <= '9') || ('A' <= c && c <= 'F'));
     }
 }
