67386ddab8dce9f698e60a0fa514835056a89eff
yeonwoo.kang <kywoo26@naver.com>
2024-05-03 14:54:14 +0900

    Add parsing input for commands

--- src/main/java/shell/TestShellScript.java
+++ src/main/java/shell/TestShellScript.java
@@ -1,16 +1,42 @@
 package shell;
 
+import org.assertj.core.util.Strings;
 import org.assertj.core.util.VisibleForTesting;
 
 import java.util.Scanner;
 
 public class TestShellScript {
+
+    private static final String READ = "read";
+    private static final String FULL_READ = "fullread";
+    private static final String EXIT = "exit";
+    private static boolean isExit = false;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
+        while (!isExit) {
             String input = scanner.nextLine();
 
+            if (!Strings.isNullOrEmpty(input)) {
+                continue;
+            }
+
+            String[] commands = input.split(" ");
+
+            switch (commands[0]) {
+                case READ:
+                    write();
+                    break;
+                case FULL_READ:
+                    fullRead();
+                    break;
+                case EXIT:
+                    exit();
+                    break;
+                default:
+                    System.out.println("Invalid commands");
+            }
         }
     }
 
@@ -25,8 +51,7 @@ public class TestShellScript {
     }
 
     @VisibleForTesting
-    static void read() {
-
+    static void read(int input) {
     }
 
     @VisibleForTesting
@@ -36,7 +61,7 @@ public class TestShellScript {
 
     @VisibleForTesting
     static void exit() {
-
+        isExit = true;
     }
 
     @VisibleForTesting
