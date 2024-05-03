e6ab99be159c271394499ade6dcb2574451f0555
jiseongyoo <jsing8@nate.com>
2024-05-03 13:38:16 +0900

    TestShellScript 스켈레톤 코드 작성함

--- src/main/java/shell/TestShellScript.java
+++ src/main/java/shell/TestShellScript.java
@@ -1,5 +1,7 @@
 package java.shell;
 
+import org.assertj.core.util.VisibleForTesting;
+
 import java.ssd.DeviceDriver;
 import java.ssd.SamsungSSD;
 
@@ -8,5 +10,35 @@ public class TestShellScript {
         DeviceDriver deviceDriver = new DeviceDriver(new SamsungSSD());
         deviceDriver.writeData(10, 10);
         deviceDriver.readData(10);
+    }
+
+    @VisibleForTesting
+    static void write() {
+
+    }
+
+    @VisibleForTesting
+    static void fullwrite() {
+
+    }
+
+    @VisibleForTesting
+    static void read() {
+
+    }
+
+    @VisibleForTesting
+    static void fullwread() {
+
+    }
+
+    @VisibleForTesting
+    static void exit() {
+
+    }
+
+    @VisibleForTesting
+    static void help() {
+
     }
 }
