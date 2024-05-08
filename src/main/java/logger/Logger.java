cd8607e82a185e68c8c17378aaced8683d6c8b53
jiseongyoo <jsing8@nate.com>
2024-05-08 16:18:16 +0900

    Log formatting 기능 구현

--- src/main/java/logger/Logger.java
+++ src/main/java/logger/Logger.java
@@ -1,11 +1,14 @@
 package logger;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
 public class Logger {
     private static final HashMap<String, Logger> loggerMap = new HashMap<>();
-    private static final String logFormat = "";
+    public static final String FORMAT_TIMESTAMP = "yy.MM.dd HH:mm";
+    public static final int METHOD_PADDING_LENGTH = 30;
     private final String logPath;
 
     private Logger(String logPath){
@@ -33,7 +36,18 @@ public class Logger {
         return new File("");
     }
 
-    public String formatting(String content) {
-        return content;
+    public String formatLogContent(String method, String content) {
+        return "[" + getFormattedTime(new Date()) + "] " +
+                getRightPaddedString(method + "()", METHOD_PADDING_LENGTH) +
+                content;
+    }
+
+    public String getFormattedTime(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy.MM.dd HH:mm");
+        return simpleDateFormat.format(date);
+    }
+
+    private String getRightPaddedString(String content, int padLength) {
+        return String.format("%1$-" + padLength + "s", content);
     }
 }
--- src/test/java/logger/LoggerTest.java
+++ src/test/java/logger/LoggerTest.java
@@ -1,7 +1,44 @@
 package logger;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 
+@ExtendWith(MockitoExtension.class)
 class LoggerTest {
+    public static final String TEST_LOG_PATH = "TestLogPath";
+    public static final String STOPPED_TIME = "24.03.11 07:12";
 
+    Logger logger;
+    Date stoppedDate;
+    String currentDate;
+
+    @BeforeEach
+    void setUp() throws ParseException {
+        logger = Logger.getInstance("TEST_LOG_PATH");
+        stoppedDate = new SimpleDateFormat(Logger.FORMAT_TIMESTAMP).parse(STOPPED_TIME);
+        currentDate = new SimpleDateFormat(Logger.FORMAT_TIMESTAMP).format(new Date());
+    }
+
+    @Test
+    void getFormattedTime() {
+        assertEquals(STOPPED_TIME, logger.getFormattedTime(stoppedDate));
+    }
+
+    @Test
+    void formatLogContent() {
+        String actual = logger.formatLogContent("formatLogContent", "Hello World!");
+
+        String expected = "[" + currentDate + "] formatLogContent()            Hello World!";
+        assertEquals(expected, actual);
+    }
 }
